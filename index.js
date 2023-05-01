c690a260f08561177d42a571be08cf9bb28aea51
sakibmahbub <arafatsakib48@gmail.com>
2023-05-01 13:34:26 +0600

    added firebase config file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const news = require("./data/news.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Dragon news is running");
+  res.send("Dragon is running");
 });
 
 app.get("/categories", (req, res) => {
@@ -27,15 +27,15 @@ app.get("/news/:id", (req, res) => {
 });
 
 app.get("/categories/:id", (req, res) => {
-  const id = req.params.id;
+  const id = parseInt(req.params.id);
   if (id === 0) {
     res.send(news);
   } else {
-    const categoryNews = news.filter((n) => parseInt(n.category_ids) === id);
+    const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
     res.send(categoryNews);
   }
 });
 
 app.listen(port, () => {
-  console.log(`Dragon api is running on port : ${port}`);
+  console.log(`Dragon API is running on port: ${port}`);
 });
